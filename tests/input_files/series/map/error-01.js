68f15e492e9119ae87bef4e5d10e643685d4798c
Chris Modzelewski <software@insightindustry.com>
2022-10-24 14:00:32 -0400

    Implemented unit tests for options.series.map.

---
+++ tests/input_files/series/map/error-01.js
@@ -0,0 +1,15 @@
+{
+  data: [
+    'invalid value'
+  ],
+
+  affectsMapView: true,
+  animationLimit: 250,
+  dataAsColumns: false,
+  negativeColor: '#000',
+  nullColor: '#ccc',
+  nullInteraction: false,
+
+  allAreas: true,
+  joinBy: 'hc-key'
+}
